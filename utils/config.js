d4a237dfbee8a023a9e3da3e2024e7e8c40c0b57
Burben <burbenburbenov@gmail.com>
2025-12-01 09:20:37 +0200

    231231

--- utils/config.js
+++ utils/config.js
@@ -1,10 +1,10 @@
 // backend/utils/config.js
-require('dotenv').config({ path: require('path').resolve(__dirname, '../.env'), debug: true })
+require("dotenv").config()
 
 const PORT = process.env.PORT || 3001
 const MONGODB_URI = process.env.MONGODB_URI
 
 module.exports = {
   PORT,
-  MONGODB_URI
+  MONGODB_URI,
 }
